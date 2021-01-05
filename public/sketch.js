4ec8d0ccde376011515328b834b8670655c0be07
Andrea Bellavita <andrea.bellavita@mail.polimi.it>
2021-01-05 11:28:08 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -5,7 +5,6 @@ let socket = io();
 var otherCursors = [];
 var particles0 = [];
 var clickEffect = [];
-var cnv;
 var palette = [
   {r: 3, g: 196, b: 216 },
   {r: 0, g: 146, b: 255 },
@@ -20,7 +19,7 @@ var palette = [
 // __ Setup __
 
 function setup() {
-  cnv = createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
   myCursor = new myCursor();
 
@@ -105,11 +104,10 @@ socket.on('deleteCursor', function(data) {
 
 function mousePos(data) {
   // Find the cursor that has the same ID of the data received
-  var getPos = otherCursors.find(otherCursor => otherCursor.id === data.id);
-
-  console.log(data.x + "/" + data.y);
   data.x = map(data.x, 0, data.win_w, 0, width, true);
   data.y = map(data.y, 0, data.win_h, 0, height, true);
+  console.log(data.x + "/" + data.y);
+  var getPos = otherCursors.find(otherCursor => otherCursor.id === data.id);
 
   // If no cursor with that ID is find ---> "getPos" is set to undefined
   // so create a new cursor with that ID
@@ -155,51 +153,51 @@ class myCursor {
   }
 }
 
-function otherCursor(temp_x, temp_y, temp_w, temp_h, temp_id){
-  push();
-
-    this.x = map(temp_x, 0, temp_w, 0, width);
-    this.y = map(temp_y, 0, temp_h, 0, height);
-
-    this.color = palette[round(random(palette.length-1))];
-    this.display = function() {
-      fill(this.color.r, this.color.g, this.color.b, 50);
-      ellipse(this.x, this.y, 100);
-    }
-  pop();
-}
-
-// function otherCursor(temp_x, temp_y, temp_w, temp_h, temp_id) {
-//   this.x = map(temp_x, 0, temp_w, 0, width);
-//   this.y = map(temp_y, 0, temp_h, 0, height);
+// function otherCursor(temp_x, temp_y, temp_w, temp_h, temp_id){
+//   push();
 //
-//   // this.x = temp_x// - width / 2;
-//   // this.y = temp_y// - height / 2;
-//   this.id = temp_id;
-//   this.color = palette[round(random(palette.length-1))];
-//   this.size = 50;
-//   this.history = [];
+//     this.x = map(temp_x, 0, temp_w, 0, width);
+//     this.y = map(temp_y, 0, temp_h, 0, height);
 //
-//   this.update = function() {
-//     var prevPos = {
-//       x: this.x,
-//       y: this.y
+//     this.color = palette[round(random(palette.length-1))];
+//     this.display = function() {
+//       fill(this.color.r, this.color.g, this.color.b, 50);
+//       ellipse(this.x, this.y, 100);
 //     }
-//     this.history.push(prevPos);
-//
-//     if (this.history.length > 40) {
-//       this.history.splice(0, 1);
-//     }
-//   }
-//
-//   this.display = function() {
-//     noStroke();
-//     fill(this.color.r, this.color.g, this.color.b, 50);
-//     for (var i = 0; i < this.history.length; i++) {
-//       ellipse(this.history[i].x, this.history[i].y, i / 1.5);
-//     }
-//   }
+//   pop();
 // }
+
+function otherCursor(temp_x, temp_y, temp_w, temp_h, temp_id) {
+  this.x = map(temp_x, 0, temp_w, 0, width);
+  this.y = map(temp_y, 0, temp_h, 0, height);
+
+  // this.x = temp_x// - width / 2;
+  // this.y = temp_y// - height / 2;
+  this.id = temp_id;
+  this.color = palette[round(random(palette.length-1))];
+  this.size = 50;
+  this.history = [];
+
+  this.update = function() {
+    var prevPos = {
+      x: this.x,
+      y: this.y
+    }
+    this.history.push(prevPos);
+
+    if (this.history.length > 40) {
+      this.history.splice(0, 1);
+    }
+  }
+
+  this.display = function() {
+    noStroke();
+    fill(this.color.r, this.color.g, this.color.b, 50);
+    for (var i = 0; i < this.history.length; i++) {
+      ellipse(this.history[i].x, this.history[i].y, i / 1.5);
+    }
+  }
+}
 
 
 class particelle {
