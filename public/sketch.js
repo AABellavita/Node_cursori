d98ce036040d9c12b8e2ed8e6b70d29b8d103bd1
Andrea Bellavita <andrea.bellavita@mail.polimi.it>
2021-01-10 17:34:23 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -39,7 +39,7 @@ function setup() {
   slider0.addImage(loadImage('assets/images/slider.png'));
   slider0.visible = true;
 
-  click0 = createSprite(width / 30 * 7, height / 30 * 7);
+  click0 = createSprite(width / 30 * 7 - width / 2, height / 30 * 7 - height / 2);
   click0.addImage(prova);
   click0.visible = true;
 
